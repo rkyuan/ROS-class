684b20c3026d52893c4df288f91c24dedea8286f
Rick <hxy153@case.edu>
2015-11-04 12:01:15 -0500

    moved pose over to lib

--- interesting_moves/src/interestingMoves.cpp
+++ interesting_moves/src/interestingMoves.cpp
@@ -7,6 +7,13 @@ interestingMoves::interestingMoves(){
 }
 void interestingMove1(trajectory_msgs::JointTrajectory &des_trajectory){
 	Vectorq7x1 q_pre_pose;
+	q_pre_pose<< -0.907528, -0.111813,   2.06622,    1.8737,    -1.295,   2.00164,  -2.87179;
+	std::vector<Eigen::VectorXd> des_path;
+	Vectorq7x1 q_vec_right_arm;
+	q_vec_right_arm =  baxter_traj_streamer.get_qvec_right_arm();
+	des_path.push_back(q_vec_right_arm);
+	des_path.push_back(q_pre_pose);
+	baxter_traj_streamer.stuff_trajectory(des_path, des_trajectory);
 
 }
 void interestingMove2(trajectory_msgs::JointTrajectory &des_trajectory){
